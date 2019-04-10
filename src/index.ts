4fd942a66d0b1447ae73f9de09dc28ce0e8381c0
Geoff Liu <hi@geoffliu.me>
2019-04-10 14:46:41 -0400

    Compilestsc

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 import * as _ from 'lodash'
 import { generateMondrian } from './generator'
 
-  /*
 const CANVAS_WIDTH = 1600
 const CANVAS_HEIGHT = 900
 const MIN_CELL_SIZE = 100
 const MIN_VERTICAL_SPLIT = MIN_CELL_SIZE / CANVAS_WIDTH
 const MIN_HORIZONTAL_SPLIT = MIN_CELL_SIZE / CANVAS_HEIGHT
-   */
 
-export default function genMondrian(canvas: Object) {
-  /*
+export default function genMondrian(canvas: HTMLCanvasElement) {
   canvas.width = CANVAS_WIDTH
   canvas.height = CANVAS_HEIGHT
 
@@ -18,13 +15,18 @@ export default function genMondrian(canvas: Object) {
   ctx.fillStyle = 'white'
   ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
 
-  gridCells.forEach(({ x, y, width, height, color }) => {
+  const {cells, lines} = generateMondrian({
+    minVerticalSplit: MIN_VERTICAL_SPLIT,
+    minHorizontalSplit: MIN_HORIZONTAL_SPLIT,
+  })
+
+  cells.forEach(({ x, y, width, height, color }) => {
     ctx.fillStyle = color
     ctx.fillRect(x * CANVAS_WIDTH, y * CANVAS_HEIGHT, width * CANVAS_WIDTH, height * CANVAS_HEIGHT)
   })
 
   ctx.fillStyle = 'black'
-  gridLines.forEach(({ horizontal, split, start, length }) => {
+  lines.forEach(({ horizontal, split, start, length }) => {
     const halfLineWidth = Math.random() * 3 + 1.5
     if (horizontal)
       ctx.fillRect(start * CANVAS_WIDTH, split * CANVAS_HEIGHT - halfLineWidth, length * CANVAS_WIDTH, halfLineWidth * 2)
@@ -35,5 +37,4 @@ export default function genMondrian(canvas: Object) {
   ctx.fillRect(0, 0, 2, CANVAS_HEIGHT)
   ctx.fillRect(CANVAS_WIDTH - 2, 0, 2, CANVAS_HEIGHT)
   ctx.fillRect(0, CANVAS_HEIGHT - 2, CANVAS_WIDTH, 2)
-   */
 }
